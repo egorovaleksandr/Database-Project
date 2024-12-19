77b3a37f67457f043075f3ad64ea4d39360633ab
helloy90 <helloy90git@yandex.ru>
2024-12-20 01:04:02 +0300

    update frontend

--- dealershipDB/main.py
+++ dealershipDB/main.py
@@ -1,7 +1,8 @@
-from frontend import Frontend
 from tkinter import Tk
 
-if __name__=='__main__':
+from frontend import Frontend
+
+if __name__ == '__main__':
     window = Tk()
     application = Frontend(window)
     window.mainloop()
